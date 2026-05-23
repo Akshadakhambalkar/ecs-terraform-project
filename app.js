195483e25d1a9b0189d05fe91efbbce4d81f5f65
AK <akshadakhambalkar1734@gmail.com>
2026-05-23 18:36:30 +0530

    Added professional UI

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
 
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-  res.send("Hello AK 🚀 ECS + Terraform + ECR Working Successfully");
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.get("/health", (req, res) => {
